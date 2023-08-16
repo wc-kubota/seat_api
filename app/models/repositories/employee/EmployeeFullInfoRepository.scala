7924a12d40cdb8beac5230e43d505bca1e02db39
kouki.kubota <kouki.kubota@webcrew.co.jp>
2023-08-16 11:26:29 +0900

    FIX: fix a tiny typo

--- app/models/repositories/employee/EmployeeFullInfoRepository.scala
+++ app/models/repositories/employee/EmployeeFullInfoRepository.scala
@@ -1,9 +1,7 @@
 package models.repositories.employee
 
 import models.entities.employee.EmployeeFullInfo
-//import models.usecases.employee.{UpdateEmployeeFullInfoInput, UpdateEmployeeInput}
-import models.usecases.employee.{ UpdateEmployeeInput}
-
+import models.usecases.employee.UpdateEmployeeFullInfoInput
 import models.vo.{Count, EmployeeId}
 
 import scala.concurrent.Future
@@ -11,6 +9,6 @@ import scala.concurrent.Future
 trait EmployeeFullInfoRepository {
   def employeeCount(): Future[Count]
   def fetchEmployeeFullInfo(): Future[Seq[EmployeeFullInfo]]
-//  def updateEmployeeFullInfo(input: UpdateEmployeeFullInfoInput, employeeId: EmployeeId): Future[Boolean]
+  def updateEmployeeFullInfo(input: UpdateEmployeeFullInfoInput, employeeId: EmployeeId): Future[Boolean]
 }
 
